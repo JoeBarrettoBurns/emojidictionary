108e6438a5dad0c8c40a69e9394830ba7c5df808
Joe B-B <joeb-b@Joes-MacBook-Pro-2.local>
2018-03-26 21:21:17 -0400

    added tableview and second table view before seque

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
 
+    @IBOutlet weak var emojtableview: UITableView!
+    
+    var emojis = ["😁","😆","🤓","😎","🤩","🥩","🍠","💎","bacon"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //look to this view controller to answer what data is in it and how much
+        emojtableview.dataSource = self
+        emojtableview.delegate = self
     }
-
+    // how many rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    // what do we want inside each cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        print(indexPath.row)
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
